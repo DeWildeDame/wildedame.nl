f8a5804f6b3ef56bd879c8750b8d12a629fe84c1
Tamara Klaassen <tamara@wildedame.nl>
2026-04-03 01:33:45 +0200

    Add some logos Add some glitch effects on social links Use svg mask for social links Tweak CVcards

--- src/data/expierence.tsx
+++ src/data/expierence.tsx
@@ -10,10 +10,10 @@ export const experience = [
 	  As the product evolved into a globally-used platform, I continued shaping the experience. After the acquisition by Recast Software, I continued developing the platform under the new brand, leading the design and release of the Microsoft Teams app.
 	`
 	}, {
-		logo: "/assets/img/cv/netflex.svg",
+		logo: "/assets/img/cv/netflex.png",
 		role: "Marketing & Web Designer",
 		company: "@Netflex",
-		companyUrl: "https://www.recastsoftware.com/",
+		companyUrl: "https://www.netflex.nl/",
 		period: "Feb 2014 – May 2016",
 		description: `
 Owned the design, build, and maintenance of the company’s website, along with a range of branded digital experiences, including customer portals and microsites.
@@ -21,7 +21,7 @@ Owned the design, build, and maintenance of the company’s website, along with 
 Working within the creative freedom to shape both the visual identity and digital outreach of the company from end to end, crafting cohesive,
 user-focused experiences that bridged design and communication.	`
 	}, {
-		logo: "/assets/img/cv/recast-software.png",
+		logo: "/assets/img/cv/static-motion.png",
 		role: "Web Designer ",
 		company: "@Static Motion",
 		companyUrl: "web.archive.org/web/20140918155229/http://www.staticmotion.nl/",
@@ -29,15 +29,12 @@ user-focused experiences that bridged design and communication.	`
 		description: `
 	  Led branding and digital design projects for small to mid-sized businesses. Created visual identities, websites, and print materials translating each client’s story into cohesive, cross-medium experiences.
 	  `
-	}, {
-		logo: "/assets/img/cv/recast-software.png",
+	}, /*{
+		logo: null,
 		role: "Web Designer @Hollander Sign & Design",
 		company: "@Hollander Sign & Design",
 		companyUrl: null,
 		period: "Sep 2006 – Mar 2009",
-		description: `
-	  Joined from day one to lead UX and front-end development of Liquit Workspace. Solely responsible for both disciplines for over six years.
-	  As the product evolved into a globally-used platform, I continued shaping the experience. After the acquisition by Recast Software, I continued developing the platform under the new brand, leading the design and release of the Microsoft Teams app.
-	`
-	},
+		description: ``
+	},*/
 ];
