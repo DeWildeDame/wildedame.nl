402474c59f27f64a5ac1a12041f8a6be9719cf8e
Tamara Klaassen <tamara@wildedame.nl>
2026-04-02 21:42:34 +0200

    Commit to remote repo

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = "Wildedame.nl | Adventurer extraordinaire, Mesh runner, and all-around awesome person.";
 export const SITE_DESCRIPTION = 'Welcome to my website!';
