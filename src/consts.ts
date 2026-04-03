cfada471023e19a1f206a29a45c219d37c1ef81d
Tamara Klaassen <tamara@wildedame.nl>
2026-04-03 16:13:34 +0200

    Fix some css issues Start work on terminal

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Wildedame.nl | Adventurer extraordinaire, Mesh runner, and all-around awesome person.";
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Welcome to my new digital home, I am Tamara! Let\'s talk!';
